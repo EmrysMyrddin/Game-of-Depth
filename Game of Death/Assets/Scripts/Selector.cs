4b89bb042ae149e5da6e7f43ffa0701fca378941
Valentin Cocaud <valentin@cocaud.com>
2015-05-09 01:25:21 +0200

    First try of selector

---
+++ Game of Death/Assets/Scripts/Selector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selector : MonoBehaviour {
+
+	private float nextTime = 0f;
+	public float updateRate =  0.0001f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetAxis ("Vertical") > 0 && Time.time >= nextTime) {
+			nextTime = Time.time + updateRate;
+			this.gameObject.transform.position = new Vector3 (this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z + 1f);
+		} else if (Input.GetAxis ("Vertical") < 0 && Time.time >= nextTime) {
+			nextTime = Time.time + updateRate;
+			this.gameObject.transform.position = new Vector3 (this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z - 1f);
+		}
+	}
+}
