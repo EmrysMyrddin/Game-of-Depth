9b8d3a0dbffc3cbb9591f12455219617ec993f77
alphasepard <Benjamin.fraquet@gmail.com>
2015-05-09 19:08:59 +0200

    les unité il se tape sur la troche

--- Game of Death/Assets/Scripts/Unit.cs
+++ Game of Death/Assets/Scripts/Unit.cs
@@ -7,6 +7,7 @@ public class Unit : MonoBehaviour {
 	public int direction;
 	public int attack;
 	public int life;
+	public string type;
 
 	private float x,y,z;
 	private Model model;
@@ -14,6 +15,7 @@ public class Unit : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		model = GameObject.Find ("Plateau").GetComponent("Model") as Model;
+		type = gameObject.tag;
 	}
 	
 	// Update is called once per frame
@@ -34,11 +36,25 @@ public class Unit : MonoBehaviour {
 		gameObject.transform.position = new Vector3 (x, y, z);
 	}
 
-	public void move()
-	{
+	public void move(){
 		modelX += direction;
 		if(modelX > model.nbColone || modelX < 0)
 			Destroy(gameObject);
 	}
 
+	bool Loose(int lifeOp, int attackOp) {
+		int me = this.attack / lifeOp;
+		int him = attackOp / this.life;
+		return (me > him) ? false : true;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		string otherType = other.gameObject.tag;
+		Unit otherObject = other.gameObject.GetComponent<Unit>();
+		if (otherType == "Soldat" || otherType == "Tank" || otherType == "Cavalier") {
+			if (otherObject.Loose(this.life, this.attack)) {
+			    Destroy(other.gameObject);
+			}
+		}
+	}
 }
