a57d174cd7e05f1bc93be73c1a97e2aab1a0bc1b
martin <martin.penard@gmail.com>
2015-05-09 11:23:50 +0200

    boutons cooldown

---
+++ Game of Death/Assets/Scripts/Icons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Icons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1") &&  GetComponent<Animator>().GetBool("clicked") == false)
+        {
+           GetComponent<Animator>().SetBool("clicked", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("clicked", false);
+        }
+        
+
+	}
+
+    
+}
