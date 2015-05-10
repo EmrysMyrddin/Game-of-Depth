7ca8f071d9a38a8fea1ecf952815848ffd6688db
alphasepard <Benjamin.fraquet@gmail.com>
2015-05-10 10:46:44 +0200

    unite sur le menu

---
+++ Game of Death/Assets/Scripts/AssassinMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssassinMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Animation anim = gameObject.GetComponent<Animation> ();
+		anim.Play ("Take 001");
+		anim.Stop ();
+		anim ["Take 001"].speed = 0;
+		anim ["Take 001"].enabled = true;
+		anim ["Take 001"].time = (1f/24f)*18;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Game of Death/Assets/Scripts/Play.cs
+++ Game of Death/Assets/Scripts/Play.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class Play : MonoBehaviour {
-
 	
     public void LoadGame(string game)
     {
